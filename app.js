90eac60de4eb38f22d56f2944c6b1161eff03f59
Ryan Rush <ryan.rush@uipath.com>
2019-12-15 09:34:43 -0800

    updates

--- app.js
+++ app.js
@@ -26,6 +26,20 @@ const httpsOptions = {
 
 const app = express();
 
+
+app.all('*', ensureSecure); // at top of routing calls
+
+
+function ensureSecure(req, res, next){
+  if(req.secure){
+    // OK, continue
+    return next();
+  };
+  // handle port numbers if you need non defaults
+  // res.redirect('https://' + req.host + req.url); // express 3.x
+  res.redirect('https://' + req.hostname + req.url); // express 4.x
+}
+
 //Decalre both http and https servers
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(httpsOptions, app);
